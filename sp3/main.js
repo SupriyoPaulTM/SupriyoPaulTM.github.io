02d92927f0014c634aa36d8a582b8956df8ea2db
SupriyoPaulTM <supriopaul911@gmail.com>
2025-11-14 20:18:14 +0530

    Last Sync: 2025-11-14 20:18 (Mobile)

--- sp3/main.js
+++ sp3/main.js
@@ -1,6 +1,21 @@
 let backToTop = document.getElementById("b2t-btn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+
+
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+};
 backToTop.addEventListener("click", topFunction);
+
+function themeToggle() {
+  document.getElementById("main").classList.toggle("dark");
+};
