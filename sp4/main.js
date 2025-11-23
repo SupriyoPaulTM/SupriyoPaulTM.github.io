d79c4fb41915b87529876883f87ccb24ace5cbd7
SupriyoPaulTM <supriopaul911@gmail.com>
2025-11-23 10:56:45 +0530

    Last Sync: 2025-11-23 10:56 (Mobile)

---
+++ sp4/main.js
@@ -0,0 +1,27 @@
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
+const menu = document.getElementById("menu");
+const overlay = document.querySelector(".overlay");
+const themeBtn = document.getElementById("theme-btn");
+
+function menuToggle() {
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+menuBtn.addEventListener("click", menuToggle);
+closeBtn.addEventListener("click", menuToggle);
+overlay.addEventListener("click", menuToggle);
+
+menu.querySelectorAll("a").forEach(function(e) {
+  e.addEventListener("click", menuToggle);
+});
+
+themeBtn.addEventListener("click", function(){
+  document.body.classList.toggle("dark-theme");
+  if (themeBtn.innerHTML == "dark_mode") {
+    themeBtn.innerHTML = "light_mode";
+  } else {
+    themeBtn.innerHTML = "dark_mode";
+  };
+});
